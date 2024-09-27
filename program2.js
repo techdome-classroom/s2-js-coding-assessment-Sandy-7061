818b57e32ee584aca65017368600a393d546072a
Sandeep <sandeepkush880@gmail.com>
2024-09-27 14:41:16 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,24 +1,28 @@
-class Solution:
-    def romanToInt(self, s: str) -> int:
-        roman_to_int = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
+var romanToInt = function(s) {
+    const roman_to_int = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+    
+    let total = 0;
+    let prev_value = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        let current_value = roman_to_int[s[i]];
+        if (current_value > prev_value) {
+            total += current_value - 2 * prev_value;
+        } else {
+            total += current_value;
         }
-        
-        total = 0
-        prev_value = 0
-        
-        for char in s:
-            current_value = roman_to_int[char]
-            if current_value > prev_value:
-                total += current_value - 2 * prev_value
-            else:
-                total += current_value
-            prev_value = current_value
-        
-        return total
+        prev_value = current_value;
+    }
+    
+    return total;
+};
+
+module.exports = { romanToInt };
