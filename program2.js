a452fb9be2d383664d84417907be7f8f851016d2
Sandeep <sandeepkush880@gmail.com>
2024-09-27 14:39:40 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -1,30 +1,10 @@
-class Solution:
-    def romanToInt(self, s: str) -> int:
-        # Define a mapping from Roman numerals to integers
-        roman_to_int = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
-        }
-        
-        total = 0
-        prev_value = 0
-        
-        # Iterate through the string of Roman numerals
-        for char in s:
-            current_value = roman_to_int[char]
-            
-            # If the current value is greater than the previous value, it indicates subtraction
-            if current_value > prev_value:
-                total += current_value - 2 * prev_value
-            else:
-                total += current_value
-                
-            # Update the previous value
-            prev_value = current_value
-        
-        return total
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    
+};
+
+
+module.exports={romanToInt}
